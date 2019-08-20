e048f9f638a8d72e4a9284b3bbb009f761dbccca
Sebastiaan de Man <sebastiaan@gmail.com>
2019-08-20 13:56:50 +0200

    run

---
+++ force-app/main/default/aura/runflow/runflowController.js
@@ -0,0 +1,49 @@
+({
+    init : function (cmp) {
+        console.log('location', window.location.search);
+        var search = location.search.substring(1);
+        //search = search.replace(/&amp;/g, '&');
+var vars = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
+console.log('vars', vars);
+       // var urlParams = new URLSearchParams(window.location.search);
+   //     var keys = urlParams.keys();
+//for(key of keys) { 
+//  console.log(key); 
+//}
+// post
+// action
+
+      var flow = cmp.find("flowData");
+      var flowName = cmp.get("v.flowName");
+      var inputVariables = [];
+/*
+ {
+          name : 'firstname',
+          type : 'String',
+          value : 'nice'
+        }
+*/
+     delete vars.flowName;
+      var ikeys = Object.keys(vars);
+      for(var key of ikeys){
+        var uvalue = unescape(vars[key]).replace(/&amp;/g, '&').replace('+', ' ');
+        inputVariables.push({
+          name : key,
+          type : 'String',
+          value : uvalue
+        });
+      }
+
+      
+      flow.startFlow(flowName, inputVariables);
+    },
+  
+    statusChange : function (cmp, event) {
+      if (event.getParam('status') === "FINISHED") {
+        //Do something
+      }
+    },
+    handleRouteChange : function(component, event, helper) {
+        console.log(event);
+    }
+  });
