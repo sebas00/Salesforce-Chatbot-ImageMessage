8574b26d4d5410ffe264080664ce82662749e0c9
Sebastiaan de Man <sebastiaan@gmail.com>
2019-08-26 11:56:33 +0200

    navigation

--- force-app/main/default/aura/runflow/runflowController.js
+++ force-app/main/default/aura/runflow/runflowController.js
@@ -1,10 +1,10 @@
 ({
     init : function (cmp) {
-        console.log('location', window.location.search);
+  //      console.log('location', window.location.search);
         var search = location.search.substring(1);
         //search = search.replace(/&amp;/g, '&');
 var vars = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) })
-console.log('vars', vars);
+//console.log('vars', vars);
        // var urlParams = new URLSearchParams(window.location.search);
    //     var keys = urlParams.keys();
 //for(key of keys) { 
@@ -24,10 +24,11 @@ console.log('vars', vars);
         }
 */
      delete vars.flowName;
+     delete vars.language;
       var ikeys = Object.keys(vars);
       for(var key of ikeys){
         var uvalue = vars[key].replace(/\+/g, ' ')
-        console.log(key, vars[key]);
+       // console.log(key, vars[key]);
         inputVariables.push({
           name : key,
           type : 'String',
@@ -45,6 +46,6 @@ console.log('vars', vars);
       }
     },
     handleRouteChange : function(component, event, helper) {
-        console.log(event);
+     //   console.log(event);
     }
   });
